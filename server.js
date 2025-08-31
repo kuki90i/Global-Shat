d0e81db8a4e63055ff6ce959449ea500bd287383
kuki90i <emalcev424@gmail.com>
2025-08-31 19:48:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Новый пользователь подключился');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Рассылка сообщения всем
+    });
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключился');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
